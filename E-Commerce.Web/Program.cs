d93b019f964ebbfbe8974ae1b8d588bdccb87cb5
Eslam631 <aslamterk784@gmail.com>
2025-04-14 19:39:09 +0200

    ProductCreate$Dbcontext

--- E-Commerce.Web/Program.cs
+++ E-Commerce.Web/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using Persistence.Data.Context;
+
 namespace E_Commerce.Web
 {
     public class Program
@@ -10,6 +13,10 @@ namespace E_Commerce.Web
             // Add services to the container.
 
             builder.Services.AddControllers();
+            builder.Services.AddDbContext<ApplicationDbContext>(Option =>
+            {
+                Option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
