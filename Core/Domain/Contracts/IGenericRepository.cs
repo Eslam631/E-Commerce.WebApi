d2eb0cceda9d1c8b22dd7f21e80e57db458051de
Eslam631 <aslamterk784@gmail.com>
2025-04-16 22:15:44 +0200

    RepositoriesAndUnitOfWork

---
+++ Core/Domain/Contracts/IGenericRepository.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Contracts
+{
+    public interface IGenericRepository<TEntity,TKey> where TEntity :BaseEntity<TKey>
+    {
+        Task< IEnumerable<TEntity>> GetAllAsync();
+
+        Task<TEntity?> GetByIdAsync(TKey key);
+         Task AddASYNC(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Remove(TEntity entity);
+
+
+
+
+    }
+}
