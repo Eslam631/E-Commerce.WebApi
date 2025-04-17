f1f9acdb567724157e5ee27d78755ec5f12ded96
Eslam631 <aslamterk784@gmail.com>
2025-04-17 15:26:38 +0200

    ProductController

--- InfraStructure/Presentation/Controllers/ProductController.cs
+++ InfraStructure/Presentation/Controllers/ProductController.cs
@@ -1,4 +1,6 @@
-﻿using ServiceAbstraction;
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceAbstraction;
+using Shared.DataTransferObject;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +9,47 @@ using System.Threading.Tasks;
 
 namespace Presentation.Controllers
 {
-   public class ProductController(IServiceManager _serviceManager)
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class ProductController(IServiceManager _serviceManager) : ControllerBase
     {
+        //Get All Product
+        [HttpGet]
+
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetaAllProduct()
+        {
+            var Products = await _serviceManager.ProductService.GetAllProductAsync();
+            return Ok(Products);
+        }
+
+
+        //Get Product By Id
+        [HttpGet("{id:int}")]
+
+
+       public async Task<ActionResult<ProductDto?>> GetProductId(int id)
+        {
+            var Product = await _serviceManager.ProductService.GetProductByIdAsync(id);
+            return Ok(Product);
+        }
+
+        //Get All Types
+        [HttpGet("types")]
+        public async Task<ActionResult<TypeProductDto>> GetAllTypes()
+        {
+            var Types=await _serviceManager.ProductService.GetAllTypeAsync();
+
+            return Ok(Types);
+        }
+
+        [HttpGet("brands")]
+
+        public async Task<ActionResult<BrandProductDto>> GetAllBrand()
+        {
+            var Brand = await _serviceManager.ProductService.GetAllBrandAsync();
+            return Ok(Brand);
+        }
+
+
     }
 }
