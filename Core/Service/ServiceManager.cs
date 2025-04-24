ca18a22a836899624642f021ef9a037311a28454
Eslam631 <aslamterk784@gmail.com>
2025-04-24 17:20:13 +0200

    BasletController

--- Core/Service/ServiceManager.cs
+++ Core/Service/ServiceManager.cs
@@ -9,9 +9,12 @@ using System.Threading.Tasks;
 
 namespace Service
 {
-    public class ServiceManager(IUnitOfWork _unitOfWork,IMapper _mapper) : IServiceManager
+    public class ServiceManager(IUnitOfWork _unitOfWork,IMapper _mapper,IBasketRepository basketRepository ) : IServiceManager
     {
         private readonly Lazy<IProductService> _serviceProvider=new Lazy<IProductService>(()=>new ProductService(_unitOfWork,_mapper));
         public IProductService ProductService => _serviceProvider.Value;
+
+        private readonly Lazy<IBasketService>  _basketService = new Lazy<IBasketService>(() => new BasketService(basketRepository, _mapper));
+        public IBasketService BasketService => _basketService.Value;
     }
 }
